e9132f401e20d4be5ecee0f69b26838ae3edcf48
Kai Lam <kailamnyc@gmail.com>
2023-08-27 23:35:14 -0400

    connected endpoints

---
+++ src/app/api/game/route.ts
@@ -0,0 +1,109 @@
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { quizCreationSchema } from "@/schemas/form/quiz";
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { ZodError } from "zod";
+
+export async function POST(req: Request, res: Response) {
+	try {
+		const session = await getAuthSession();
+		if (!session?.user) {
+			return NextResponse.json(
+				{
+					error: "you muse be logged in",
+				},
+				{ status: 401 }
+			);
+		}
+
+		const body = await req.json();
+		const { amount, topic, type } = quizCreationSchema.parse(body);
+
+		//CREATING THE GAME IN OUR SQL DB
+		const game = await prisma.game.create({
+			data: {
+				gameType: type,
+				timeStarted: new Date(),
+				//COMING FROM NEXTAUTH GOOGLE SESSION
+				userId: session.user.id,
+				topic,
+			},
+		});
+		//GRABBING QUESTIONS FROM GPT
+		const { data } = await axios.post(`${process.env.API_URL}/api/questions`, {
+			amount,
+			topic,
+			type,
+		});
+
+		if (type === "mcq") {
+			type mcQuestion = {
+				question: string;
+				answer: string;
+				option1: string;
+				option2: string;
+				option3: string;
+			};
+			//ARRAY OF GAMES TO CREATE IN DB
+			let gptQuestions = data.questions.map((question: mcQuestion) => {
+				//SHUFFLING OPTIONS
+				let options = [
+					question.answer,
+					question.option1,
+					question.option2,
+					question.option3,
+				];
+				options = options.sort(() => Math.random() - 0.5);
+
+				//MAPPING OVER GPT QUESTIONS AND RETURNING RESPONSE
+				return {
+					question: question.question,
+					answer: question.answer,
+					options: JSON.stringify(options),
+					gameId: game.id,
+					questionType: "mcq",
+				};
+			});
+			//CREATING GAMES WITH OPTIONS ABOVE
+			await prisma.question.createMany({
+				data: gptQuestions,
+			});
+		} else if (type === "open_ended") {
+			type openQuestion = {
+				question: string;
+				answer: string;
+			};
+
+			let gptQuestions = data.questions.map((question: openQuestion) => {
+				return {
+					quesion: question.question,
+					answer: question.answer,
+					gameId: game.id,
+					gameType: "open_ended",
+				};
+			});
+			await prisma.game.createMany({
+				data: gptQuestions,
+			});
+		}
+
+		return NextResponse.json({
+			gameId: game.id,
+		});
+	} catch (error) {
+		if (error instanceof ZodError) {
+			return NextResponse.json({ error: error.issues }, { status: 400 });
+		}
+
+		//MOST LIKELY ERROR WITH GPT REPONSE NOT BEING PROPER JSON
+		return NextResponse.json(
+			{
+				error: "Something went wrong",
+			},
+			{
+				status: 500,
+			}
+		);
+	}
+}
--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,4 +1,5 @@
 import { strict_output } from "@/lib/gpt";
+import { getAuthSession } from "@/lib/nextauth";
 import { quizCreationSchema } from "@/schemas/form/quiz";
 import { ZodError } from "zod";
 
@@ -7,20 +8,58 @@ import { NextResponse } from "../../../../node_modules/next/server";
 
 export const POST = async (req: Request, res: Response) => {
 	try {
+		//PROTECTING ENDPOINT FROM UNAUTHORIZED USERS
+		const session = await getAuthSession();
+		if (!session?.user) {
+			return NextResponse.json(
+				{ error: "You must be logged in to create a quiz" },
+				{ status: 401 }
+			);
+		}
+
 		const body = await req.json();
 
 		//USING ZOD SCHEMA AND GIVEN PARSE FUNCTION TO CHECK TYPE
 		const { amount, topic, type } = quizCreationSchema.parse(body);
+		//DECLARING FOR LATER
 		let questions: any;
 		if (type === "open_ended") {
-			//SYSTEM PROMPT, USER PROMPT, OUTPUT FORMAT
+			//SYSTEM PROMPT, USER PROMPT(ARRAY OF QUESTIONS), OUTPUT FORMAT
 			//WILL KEEP RECURSIVELY CALLING UNTIL RECEIVES VALID RESPONSE
 			questions = await strict_output(
-				"You are a helpful AI that is able to generate a pair of questions and answers, the length of the answer should not exceed 15 words, store all the pairs of answes and questions in a JSON array",
-				`You are to generate a random hard open-ended question about the ${topic}`,
+				"You are a helpful AI that is able to generate a pair of questions and answers, the length of the answer should not exceed 15 words, store all the pairs of answers and questions in a JSON array",
+				//CREATING AN ARRAY BASED ON THE AMOUNT PASSED AND FILLING THEM WITH SAME VALUE
+				new Array(amount).fill(
+					`You are to generate a random hard open-ended question about the ${topic}`
+				),
+
 				{
 					question: "question",
 					answer: "answer with max length of 15 words",
+				}
+			);
+			return NextResponse.json(
+				{
+					questions,
+				},
+				{
+					status: 200,
+				}
+			);
+		} else if (type === "mcq") {
+			questions = await strict_output(
+				"You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not exceed 15 words.",
+				//CREATING AN ARRAY BASED ON THE AMOUNT PASSED AND FILLING THEM WITH SAME VALUE
+				new Array(amount).fill(
+					`You are to generate a random mcq question about the ${topic}`
+				),
+
+				{
+					question: "question",
+					answer: "answer with max length of 15 words",
+					option1: "1st option with max length of 15 words",
+					option2: "2nd option with max length of 15 words",
+					option3: "3rd option with max length of 15 words",
 				}
 			);
 			return NextResponse.json(
