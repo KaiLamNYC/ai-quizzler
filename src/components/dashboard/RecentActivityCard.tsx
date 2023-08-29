523a4c922bc8b9af0126a69df7830adad105c151
Kai Lam <kailamnyc@gmail.com>
2023-08-29 17:58:36 -0400

    finished user recent activity and history

--- src/components/dashboard/RecentActivityCard.tsx
+++ src/components/dashboard/RecentActivityCard.tsx
@@ -1,4 +1,9 @@
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
+import HistoryComponent from "../HistoryComponent";
 import {
 	Card,
 	CardContent,
@@ -8,15 +13,31 @@ import {
 } from "../ui/card";
 type Props = {};
 
-const RecentActivityCard = (props: Props) => {
+const RecentActivityCard = async (props: Props) => {
+	const session = await getAuthSession();
+	if (!session?.user) {
+		return redirect("/");
+	}
+
+	const gamesCount = await prisma.game.count({
+		where: {
+			userId: session.user.id,
+		},
+	});
+
 	return (
 		<Card className='col-span-4 lg:col-span-3'>
 			<CardHeader>
-				<CardTitle className='text-2xl font-bold'>Recent Activity</CardTitle>
-				<CardDescription>You have taken a total of 8 quizzes</CardDescription>
+				<CardTitle className='text-2xl font-bold'>
+					<Link href='/history'>Recent Activity</Link>
+				</CardTitle>
+				<CardDescription>
+					You have taken a total of {gamesCount} quizzes
+				</CardDescription>
 			</CardHeader>
 			<CardContent className='max-h-[580px] overflow-scroll'>
-				History
+				{/* KNOWN TS ERROR JUST HAVE TO UPDATE TO LATEST TS */}
+				<HistoryComponent limit={10} userId={session.user.id} />
 			</CardContent>
 		</Card>
 	);
